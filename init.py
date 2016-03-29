a42705beeb1a3b896f82aa14b8695a2bda5c8f2e
lhao77 <lhao77@sohu.com>
2016-03-30 00:56:16 +0800

    修改表结构； replace的bug； 下载数据。

--- init.py
+++ init.py
@@ -18,6 +18,63 @@ g_fetch_time = 18
 g_mktidxd = 'mktidxd'
 g_mktequd = 'mktequd'
 
+g_datayest_mktadjf = 'datayest_mktadjf'
+g_update_config = 'UPDATE `_config` SET `last_daysdata_update_date`=\'%s\' WHERE (`last_daysdata_update_date`=\'%s\')'
+
+g_create_table_mktequd = 'CREATE TABLE IF NOT EXISTS `%s` ( \
+  `secID` text,\
+  `ticker` bigint(20) DEFAULT NULL,\
+  `secShortName` text,\
+  `exchangeCD` text,\
+  `tradeDate` varchar(255) NOT NULL DEFAULT \'\',\
+  `preClosePrice` double DEFAULT NULL,\
+  `actPreClosePrice` double DEFAULT NULL,\
+  `openPrice` double DEFAULT NULL,\
+  `highestPrice` double DEFAULT NULL,\
+  `lowestPrice` double DEFAULT NULL,\
+  `closePrice` double DEFAULT NULL,\
+  `turnoverVol` bigint(20) DEFAULT NULL,\
+  `turnoverValue` double DEFAULT NULL,\
+  `dealAmount` double DEFAULT NULL,\
+  `turnoverRate` double DEFAULT NULL,\
+  `accumAdjFactor` double DEFAULT NULL,\
+  `negMarketValue` double DEFAULT NULL,\
+  `marketValue` double DEFAULT NULL,\
+  `PE` double DEFAULT NULL,\
+  `PE1` double DEFAULT NULL,\
+  `PB` double DEFAULT NULL,\
+  `isOpen` bigint(20) DEFAULT NULL,\
+  PRIMARY KEY (`tradeDate`)\
+) ENGINE=InnoDB DEFAULT CHARSET=utf8'
+
+g_create_table_mktidxd = 'CREATE TABLE IF NOT EXISTS `%s` (\
+  `indexID` text,\
+  `ticker` varchar(20) DEFAULT NULL,\
+  `porgFullName` text,\
+  `secShortName` text,\
+  `exchangeCD` text,\
+  `tradeDate` varchar(255) NOT NULL DEFAULT \'\',\
+  `preCloseIndex` double DEFAULT NULL,\
+  `openIndex` double DEFAULT NULL,\
+  `lowestIndex` double DEFAULT NULL,\
+  `highestIndex` double DEFAULT NULL,\
+  `closeIndex` double DEFAULT NULL,\
+  `turnoverVol` double DEFAULT NULL,\
+  `turnoverValue` double DEFAULT NULL,\
+  `CHG` double DEFAULT NULL,\
+  `CHGPct` double DEFAULT NULL,\
+  PRIMARY KEY (`tradeDate`)\
+) ENGINE=InnoDB DEFAULT CHARSET=utf8'
+
+g_insert_table_mktequd = 'INSERT INTO %s (secID, ticker, secShortName, exchangeCD, tradeDate, \
+preClosePrice, actPreClosePrice, openPrice, highestPrice, lowestPrice, closePrice, turnoverVol, \
+turnoverValue, dealAmount, turnoverRate, accumAdjFactor, negMarketValue, marketValue, PE, PE1, PB, isOpen) \
+VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \
+\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\')'
+
+g_insert_table_mktidxd = 'INSERT INTO `%s` VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', %s, %s, \
+ %s, %s, %s, %s, %s, %s, %s)'
+
 def initDb():
     global g_engine
     global g_conn
